e1aa8d9bd4341835c53a2ffb7d42a3033fca9b57
AxelMatthew12 <143260705+AxelMatthew12@users.noreply.github.com>
2024-10-27 20:05:32 +0700

    Update_username or password error handle

--- index/proses_login.php
+++ index/proses_login.php
@@ -9,12 +9,13 @@
         //validasi Nama
 
         if (empty($username)) {
-            $errors[] = "Username tidak boleh kosong";
+            echo "Username tidak boleh kosong <br>";
         }
 
 //        validasi password
         if (empty($password)) {
-            $errors[] = "Password tidak boleh kosong";
+            echo"Password tidak boleh kosong <br>";
+            echo "<a href='form_loginPengguna.php'>Login Ulang</a>";
         }
     }
 
@@ -24,17 +25,10 @@
        }
    }
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
     $errors = array();
-
-    //validasi Nama
-
-    if (empty($username)) {
-        $errors[] = "Username tidak boleh kosong";
-    }
 
     if (empty($error)) {
         if ($username == "admin" && $password == "admin123") {
@@ -42,14 +36,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['username'] = $username;
             $_SESSION['status'] = 'login';
             echo "Anda berhasil login. Silahkan menuju <a href='index.php'>Halaman Utama</a>";
-        }else {
-            echo "Username atau Password salah <a href='form_loginPengguna.php'>Klik Untuk LoginUlang</a>";
-        }
-
-
-//        validasi password
-        if (empty($password)) {
-            $errors[] = "Password tidak boleh kosong";
         }
     }
 }
